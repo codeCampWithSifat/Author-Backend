34ae370e167272454916a29d0035f8d71615bbd6
codeCampWithSifat <sayedhossainsifat300@.com>
2024-08-03 02:31:40 +0600

    not route handler are done

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
+// import { NextFunction, Request, Response } from 'express';
+// import httpStatus from 'http-status';
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: 'API Not Found !!',
-    error: '',
-  });
-};
+// const notFound = (req: Request, res: Response, next: NextFunction) => {
+//   return res.status(httpStatus.NOT_FOUND).json({
+//     success: false,
+//     message: 'API Not Found !!',
+//     errorMessages: [
+//       {
+//         path: req.originalUrl,
+//         message: 'API Not Found',
+//       },
+//     ],
+//   });
+// };
 
-export default notFound;
+// export default notFound;
